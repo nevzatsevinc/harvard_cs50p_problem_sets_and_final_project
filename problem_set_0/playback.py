229dfebb9639f77b9793d7f8d80c809c860cd848
nevzatsevinc <ns27tr@gmail.com>
2023-09-16 23:34:34 +0200

    Added Problem Set 0 - playback.py

--- problem_set_0/playback.py
+++ problem_set_0/playback.py
@@ -3,3 +3,11 @@ Problem Set 0 - Playback Speed
 https://cs50.harvard.edu/python/2022/psets/0/playback/
 """
 
+# Prompt the user for input
+user_input = input("Enter some text: ")
+
+# Replacing each space with ...
+slow_user_input = user_input.replace(" ", "...")
+
+# Print the replaced input
+print(slow_user_input)
