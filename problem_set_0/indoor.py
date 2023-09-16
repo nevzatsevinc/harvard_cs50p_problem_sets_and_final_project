2f0a9ff351b481aef289eb2395fc7841005483c0
nevzatsevinc <ns27tr@gmail.com>
2023-09-16 23:14:47 +0200

    Added Problem Set 0 - indoor.py

---
+++ problem_set_0/indoor.py
@@ -0,0 +1,13 @@
+"""
+Problem Set 0 - Indoor Voice
+https://cs50.harvard.edu/python/2022/psets/0/indoor/
+"""
+
+# Prompt the user for input
+user_input = input("Enter some text: ")
+    
+# Convert the input to lowercase
+lowercase_input = user_input.lower()
+    
+# Print the lowercase input
+print(lowercase_input)
---
+++ problem_set_0/playback.py
@@ -0,0 +1,5 @@
+"""
+Problem Set 0 - Playback Speed
+https://cs50.harvard.edu/python/2022/psets/0/playback/
+"""
+
