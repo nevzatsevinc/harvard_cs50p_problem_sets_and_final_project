9150c3d8cd5a5cf753300239756ab848745482e8
nevzatsevinc <ns27tr@gmail.com>
2023-09-17 00:07:25 +0200

    Added Problem Set 0 - einstein.py

---
+++ problem_set_0/einstein.py
@@ -0,0 +1,14 @@
+"""
+Problem Set 0 - Einstein
+https://cs50.harvard.edu/python/2022/psets/0/einstein/
+"""
+
+# Prompt the user for mass as an integer (in kilograms)
+mass = int(input("Enter mass in kilograms: "))
+
+# Calculate energy using E = mc^2
+c = 300000000
+energy = mass * c ** 2
+
+# Print the energy
+print(energy)
